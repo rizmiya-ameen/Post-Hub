0fc7bacc553f54bd5d885f8790c3ff59231fd38e
rizmiya-ameen <rizmiyaamin@gmail.com>
2023-10-20 15:47:13 +1100

    Next-Auth added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,13 +2,38 @@
 
 import Link from "next/link"
 import { useSession, signOut } from "next-auth/react"
+import Image from "next/image"
+import { useEffect, useRef, useState } from "react"
 
 const NavBar = () => {
 
-  const { status } = useSession()
+  const { status, data: session } = useSession()
+  const [ isPopupVisible, setIsPopupVisible] = useState(false)
+  const popupRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      //checking whether popupRef is set to a DOM element, then checking, if the click occured was outside the popupRef, and if its true setting the value to false
+      if(popupRef.current && !popupRef.current.contains(e.target as Node)) {
+        setIsPopupVisible(false)
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside)
+
+    if(!isPopupVisible) {
+      document.removeEventListener("click", handleClickOutside)
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside)
+    }
+  }, [isPopupVisible])
+
+
 
   return (
-    <nav className="flex justify-between pb-4 border-b mb-4">
+    <nav className="flex justify-between pb-4 border-b mb-4 relative">
       <div>
         <Link href="/">Posting Site</Link>
       </div>
@@ -16,14 +41,54 @@ const NavBar = () => {
       {
         status === 'authenticated' ? 
         (
-          <div>
-            <button 
-              onClick={() => signOut()}
-              className="bg-slate-300 px-4 py-2 rounded-md font-semibold hover:scale-105 transition"
-            >
-              Sign Out
-            </button>
-          </div>
+          <>
+            <div ref={popupRef}
+            className={`absolute z-30 right-0 top-20 bg-white p-6 shadow-lg rounded-md flex-col gap-2 text-right min-w-[160px] ${isPopupVisible ? "flex" : "hidden"}`}>
+
+              <div className="font-bold">{session?.user?.name}</div>
+
+              <div>{session?.user?.email}</div>
+
+              <Link href='/dashboard' className="hover:underline" onClick={() => setIsPopupVisible(false)}> 
+                Dashboard
+              </Link>
+
+              <Link href='/create-post' className="hover:underline" onClick={() => setIsPopupVisible(false)}> 
+                Create Post
+              </Link>
+
+              <button 
+                onClick={() => signOut()}
+                className="bg-slate-300 px-4 py-2 rounded-md font-semibold hover:scale-105 transition"
+              >
+                Sign Out
+              </button>
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <Link 
+                href="create-post"
+                className="hidden md:flex gap-2 items-center mr-6"
+              >
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                </span>
+
+                <span>Create new</span>
+              </Link>
+
+              <Image 
+                src={session?.user?.image || ""}
+                alt="Profile Image"
+                width={36}
+                height={36}
+                className="rounded-full cursor-pointer"
+                onClick={() => setIsPopupVisible(prev => !prev)}
+              />
+            </div>
+          </>
         ) : (
           <div className="flex items-center">
            <Link href='/sign-in'>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images:{
-    domains: ["images.pexels.com"],
+    domains: ["images.pexels.com", "lh3.googleusercontent.com", "avatars.githubusercontent.com"],
   },
 }
 
